76f05d1ebd5bee8d5e5ece1178d6f592fee66b0b
Greg Franz <168317593+BuildAnythingWithCode@users.noreply.github.com>
2026-05-17 00:51:31 +0800

    create initial javascript logic

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const submitRouteBtn = document.querySelector('.main-button');
+let accountExists;
+
+submitRouteBtn.addEventListener('click', function () {
+  if (accountExists) {
+    alert(
+      'Thank you for your interest in Philippines Maps! We hope to offer the ability to submit routes soon.',
+    );
+  } else {
+    alert('You must create an account in order to submit a route.');
+  }
+});
