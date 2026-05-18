397ab2b586cf3e9fd85306e48626180bc117b081
Greg Franz <168317593+BuildAnythingWithCode@users.noreply.github.com>
2026-05-18 23:41:32 +0800

    add leaflet quickstart guide code, improve minor UI

--- script.js
+++ script.js
@@ -12,3 +12,11 @@ submitRouteBtn.addEventListener('click', function () {
     alert('Account creation functionality will be added soon!');
   }
 });
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
